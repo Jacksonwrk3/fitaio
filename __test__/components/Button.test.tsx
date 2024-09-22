3e8a80d80d73c0dbc99eab1c0282978c63fd8297
Jackson <Jacksonwrk3@gmail.com>
2024-09-22 15:32:16 -0400

    Wrote tests for button

---
+++ __test__/components/Button.test.tsx
@@ -0,0 +1,36 @@
+import { render, screen } from "@testing-library/react";
+
+import { Button } from "../../src/app/components/index";
+describe("Button", () => {
+  it("should render the button", () => {
+    render(<Button>Test</Button>);
+    const button = screen.getByText("Test");
+    expect(button).toBeInTheDocument();
+  });
+
+  it("should call the onClick function when the button is clicked", () => {
+    const onClick = jest.fn();
+    render(<Button onClick={onClick}>Test</Button>);
+    const button = screen.getByText("Test");
+    button.click();
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("should render the button with the primary variant", () => {
+    render(<Button variant="primary">Primary Button</Button>);
+    const button = screen.getByText("Primary Button");
+    expect(button).toHaveClass("bg-blue-600 text-white");
+  });
+
+  it("should render the button with the outlined variant", () => {
+    render(<Button variant="outlined">Outlined Button</Button>);
+    const button = screen.getByText("Outlined Button");
+    expect(button).toHaveClass("bg-white border-[#e2e8f0] border text-black");
+  });
+
+  it("should render the button with the primary variant if variant is not provided", () => {
+    render(<Button>Test</Button>);
+    const button = screen.getByText("Test");
+    expect(button).toHaveClass("bg-blue-600 text-white");
+  });
+});
