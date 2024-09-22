14bb81a3a55661528d369fa9ff07c5c752699757
Jackson <Jacksonwrk3@gmail.com>
2024-09-22 18:00:50 -0400

    Added width prop to button. Refactored documentation to reflect changes

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -4,33 +4,39 @@
  * @description Type for the Button component's props
  * @property {React.ReactNode} children - The content inside the button
  * @property {() =>{}} onClick - The function to be called when the button is clicked
+ * @property {string} variant - The variant of the button
+ * @property {string} width - The width of the button
  */
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
   variant?: "primary" | "outlined";
+  width?: "full" | "auto";
 };
 
 /**
- * @component
- * @param {React.ReactNode} children - The content inside the button
- * @param {() => {}} onClick - The function to be called when the button is clicked
- * @param {string} variant - The variant of the button
+ * @component Button
+ * @type {React.FC<ButtonProps>}
+ * @dev - Width defaults to auto
+ * - Width[full] - Sets the width to 100%
+ * @dev - Variant defaults to primary
  * @returns {JSX.Element} The rendered button element
  */
 const Button: React.FC<ButtonProps> = ({
   children,
   onClick,
   variant = "primary",
+  width = "auto",
 }) => {
   const primaryClasses = "bg-blue-600 text-white";
-  const secondaryClasses = "bg-white border-[#e2e8f0] border text-black";
+  const secondaryClasses = "bg-white border-grayPrimary border text-black";
+  const widthClasses = width === "full" ? "w-full" : "w-auto";
   return (
     <button
       onClick={onClick}
       className={`${
         variant === "primary" ? primaryClasses : secondaryClasses
-      }  px-4 py-2 rounded`}
+      }  ${widthClasses} px-4 py-2 rounded`}
     >
       {children}
     </button>
