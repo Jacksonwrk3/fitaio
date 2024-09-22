aca9daba89196c9d2354684de4387fc680332326
Jackson <Jacksonwrk3@gmail.com>
2024-09-22 17:59:20 -0400

    added media query xs:480px

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,24 +9,10 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          100: "#d4eaf7",
-          200: "#b6ccd8",
-          300: "#3b3c3d",
-        },
-        accent: {
-          100: "#71c4ef",
-          200: "#00668c",
-        },
-        text: {
-          100: "var(--text-100)",
-          200: "var(--text-200)",
-        },
-        bg: {
-          100: "var(--bg-100)",
-          200: "var(--bg-200)",
-          300: "var(--bg-300)",
-        },
+        grayPrimary: "#e2e8f0",
+      },
+      screens: {
+        xs: "480px",
       },
     },
   },
