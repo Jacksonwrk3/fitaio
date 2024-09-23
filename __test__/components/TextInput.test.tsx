41ff6a8ce3fdce636a11eaf34e5ececb2b8371fc
Jackson <Jacksonwrk3@gmail.com>
2024-09-22 22:03:35 -0400

    Added TODO to TextInput Test

--- __test__/components/TextInput.test.tsx
+++ __test__/components/TextInput.test.tsx
@@ -1,4 +1,7 @@
 import { TextInput } from "@/app/components";
 import { render, screen, fireEvent } from "@testing-library/react";
 
+/**
+ * @TODO Add tests for TextInput
+ */
 describe("TextInput", () => {});
