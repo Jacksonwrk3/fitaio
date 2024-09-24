f5fe74e0209809c597780d67c6311dff51052900
Jackson <Jacksonwrk3@gmail.com>
2024-09-24 13:05:27 -0400

    Supabase SSR set up complete (May not be working, need testing after google auth is created)

---
+++ src/app/api/auth/signup/google/route.ts
@@ -0,0 +1,3 @@
+export async function POST() {
+  return new Response("Google account created");
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+import { on } from "events";
+import { Button, TextInput } from "../../components/index";
+import Link from "next/link";
+import React, { useState } from "react";
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const usernameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const passwordOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="max-w-lg w-full">
+        <h1 className=" font-bold text-4xl text-center">Log In</h1>
+        <form className="border-2 border-grayPrimary rounded px-6 py-8 m-2  space-y-6">
+          <TextInput
+            id="username"
+            value={username}
+            onChange={(e) => {
+              usernameOnChange(e);
+            }}
+          >
+            Username
+          </TextInput>
+          <TextInput
+            type="password"
+            id="username"
+            value={password}
+            onChange={(e) => {
+              passwordOnChange(e);
+            }}
+          >
+            Password
+          </TextInput>
+          <div className="space-y-2 flex flex-col">
+            <Button width="full">Sign Up</Button>
+            <p className="text-center">
+              Dont have an account?{" "}
+              <span className="underline">
+                <Link href="/signup">Sign up</Link>
+              </span>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
