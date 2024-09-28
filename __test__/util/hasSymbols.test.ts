8ac23724a100cb5699d59d4e7149c5d1754e7fd4
Jackson <Jacksonwrk3@gmail.com>
2024-09-28 10:38:16 -0400

    wrote tests for hasUppercase

--- __test__/util/hasSymbols.test.ts
+++ __test__/util/hasSymbols.test.ts
@@ -1,6 +1,9 @@
 import { hasSymbols } from "@/app/util/auth";
 
 describe("hasSymbols", () => {
+  /**
+   * @type {Validation Result} - The type being referenced in both test cases.
+   */
   it("isValid should be true and error should be null, if password contains at least 1 symbol", () => {
     const testPassword = "Password123!";
     const result = hasSymbols(testPassword);
---
+++ __test__/util/hasUppercase.test.ts
@@ -0,0 +1,18 @@
+import { hasUppercase } from "@/app/util/auth";
+import { ValidationResult } from "@/app/util/auth/types";
+describe("hasUppercase", () => {
+  /**
+   * @type {ValidationResult} is being referenced in both test cases
+   */
+  it("isValid should be true and error should be null if the string contains atleast 1 uppercase letter", () => {
+    const res = hasUppercase("Password123!");
+    expect(res.isValid).toEqual(true);
+    expect(res.error).toEqual(null);
+  });
+
+  it("isValid should be false and error should be `Password must contain atleast 1 symbol` if password does NOT contain an uppercase letter ", () => {
+    const res = hasUppercase("password123!");
+    expect(res.isValid).toEqual(false);
+    expect(res.error).toEqual(`Password must contain atleast 1 symbol`);
+  });
+});
