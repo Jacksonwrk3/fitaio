b1a12a8fbe0fb3f1b4bc875e6d99e0d9575c10d5
Jackson <Jacksonwrk3@gmail.com>
2024-09-28 11:21:02 -0400

    wrote and documented validLength util function

--- src/app/util/validation/validLength.ts
+++ src/app/util/validation/validLength.ts
@@ -1,5 +1,23 @@
+import { ValidationResult } from "./types";
 
-const validLength = (password: string, len:number) {
+/**
+ * @function validLength - Checks if the password is ATLEAST equal to the provided length
+ * @param {string} - The password being checked
+ * @param {len} = The minimum length the password should be
+ * @returns {ValidationResult}
+ */
+const validLength = (password: string, len: number): ValidationResult => {
+  if (password.length >= len) {
+    return {
+      isValid: true,
+      error: null,
+    };
+  } else {
+    return {
+      isValid: false,
+      error: `Password should be atleast ${len} characters long`,
+    };
+  }
+};
 
-
-}
+export default validLength;
