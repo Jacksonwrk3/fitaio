129ec3130cc53594175589188009811d87b89bb0
Jackson <Jacksonwrk3@gmail.com>
2024-10-01 12:14:41 -0400

    removed supabase client from /home

--- src/app/(Landing)/page.tsx
+++ src/app/(Landing)/page.tsx
@@ -3,8 +3,6 @@ import { Button } from "../components/index";
 import Link from "next/link";
 import { createClient } from "../util/supabase/client";
 export default function Home() {
-  const supabase = createClient();
-
   return (
     <div className="w-screen h-screen flex items-center justify-center">
       <div className="flex space-x-4">
