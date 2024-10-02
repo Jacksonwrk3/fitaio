b8d9f6218eefbf0738d4a2bb1bd567b9d0c1e7e8
Jackson <Jacksonwrk3@gmail.com>
2024-10-02 14:44:56 -0400

    Added content to (Main layout).

---
+++ src/app/(Main)/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "../globals.css";
+export const metadata: Metadata = {
+  title: "Dashboard - FitAIO",
+  description: "Main content for personal and coaching users",
+};
+
+export default function MainLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>Routines</li>
+          <li>Programs</li>
+          <li>Find a Coach</li>
+          <li>Become a Coach</li>
+        </ul>
+      </nav>
+      <main>{children}</main>
+    </div>
+  );
+}
