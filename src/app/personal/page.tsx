2ac448e67d7e54aa306ab9bd10ba044eb3e0d673
Jackson <Jacksonwrk3@gmail.com>
2024-10-02 00:17:19 -0400

    created blank personal page/route

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -1,5 +1,29 @@
+"use client";
+import { useEffect, useContext } from "react";
+import { useRouter } from "next/navigation";
+import { SessionContext } from "../context/Session/SessionContext";
+import { Button } from "../components";
+import { createClient } from "../util/supabase/client";
 const Personal = () => {
-  return <>This is the personal page</>;
+  const session = useContext(SessionContext);
+  const router = useRouter();
+  const supabase = createClient();
+  const signOut = async () => {};
+  useEffect(() => {
+    //If there is an active session, do nothing
+    if (session) {
+    }
+    //If session is null, redirect to home page ("/")
+    else {
+      router.replace("/");
+    }
+  }, [session, router]);
+  return (
+    <>
+      This is the personal page
+      <Button onClick={}>Sign out</Button>
+    </>
+  );
 };
 
 export default Personal;
