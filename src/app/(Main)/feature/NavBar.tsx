1950fed1b0512309a8e737248ca1600523f1786f
Jackson <Jacksonwrk3@gmail.com>
2024-10-06 20:35:27 -0400

    added signout to navbar

--- src/app/(Main)/feature/NavBar.tsx
+++ src/app/(Main)/feature/NavBar.tsx
@@ -1,9 +1,20 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "@/app/components";
+import { createClient } from "@/app/util/supabase/client";
 
+/**
+ * NavBar component renders a navigation bar with links and a sign-out button.
+ */
 const NavBar = () => {
+  // Get the current pathname from Next.js router
   const pathname = usePathname();
+
+  // Initialize Supabase client
+  const supabase = createClient();
+
+  // Navigation items to be displayed in the navbar
   const navItems = [
     { name: "Routine", path: "/client/routines" },
     { name: "Programs", path: "/client/programs" },
@@ -11,8 +22,26 @@ const NavBar = () => {
     { name: "Be a Coach", path: "/client/be-a-coach" },
   ];
 
+  // CSS classes for active and passive links
   const activeLinkClasses = "text-black font-bold";
   const passiveLinkClasses = "text-white font-normal";
+
+  /**
+   * Sign out the current user from Supabase.
+   *
+   * @async
+   * @function signout
+   * @returns {Promise<void>}
+   * @throws {Error} Throws an error if sign out fails.
+   */
+  const signout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      const e = error as Error;
+      console.log(e);
+      throw new Error(e.message);
+    }
+  };
 
   return (
     <nav className="bg-blue-100 p-4">
@@ -30,6 +59,7 @@ const NavBar = () => {
             </Link>
           </li>
         ))}
+        <Button onClick={signout}>Sign Out</Button>
       </ul>
     </nav>
   );
