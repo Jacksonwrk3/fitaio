31ad7950764da850f5a5aa6f45c1906ab7aed510
Jackson <Jacksonwrk3@gmail.com>
2024-10-08 14:12:15 -0400

    Changed return type of functiion from boolean to ValidationResult

--- src/app/util/validation/hasLowercase.ts
+++ src/app/util/validation/hasLowercase.ts
@@ -1,11 +1,21 @@
+import { ValidationResult } from "./types";
+
 /**
  * Checks if the given string contains any lowercase letters.
  *
  * @param {string} str - The string to check.
- * @returns {boolean} - Returns true if the string contains a lowercase letter, otherwise false.
+ * @returns {ValidationResult}
  */
-const hasLowercase = (str: string): boolean => {
-  return /[a-z]/.test(str);
+const hasLowercase = (str: string): ValidationResult => {
+  const res = /[a-z]/.test(str);
+  if (res) {
+    return { isValid: true, error: null };
+  } else {
+    return {
+      isValid: false,
+      error: "Password must contain atleast 1 lowercase character",
+    };
+  }
 };
 
 export default hasLowercase;
