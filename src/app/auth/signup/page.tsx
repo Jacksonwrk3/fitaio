1b9bfd376d6dc4c46a6de23f0a459db331d5cc62
Jackson <Jacksonwrk3@gmail.com>
2024-10-08 14:41:29 -0400

    Changed most outter div from w-screen/h-screen to min-w-screen and min-h-screen

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -31,7 +31,7 @@ const SignUp = () => {
     }
   };
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
+    <div className="min-w-screen min-h-screen flex items-center justify-center">
       <div className="max-w-lg w-full">
         <h1 className=" font-bold text-4xl text-center">Create An Account</h1>
         <form className="border-2 border-grayPrimary rounded px-6 py-8 m-2  space-y-6">
