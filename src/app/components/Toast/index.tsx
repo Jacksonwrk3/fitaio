cac21e8c837007d69a9bf119665f20e0bbae58ba
Jackson <Jacksonwrk3@gmail.com>
2024-10-16 18:03:40 -0400

    Created ToastProps interface and wrote jsdoc3 above

--- src/app/components/Toast/index.tsx
+++ src/app/components/Toast/index.tsx
@@ -1,3 +1,17 @@
-const Toast = () => {};
+import React from "react";
+
+/**
+ * @description ToastProps
+ * @property {"success" | "warning" | "error" | "info"} status
+ * @property {string} title
+ * @property {string} description
+ */
+interface ToastProps {
+  status: "success" | "warning" | "error" | "info";
+  title: string;
+  description?: string;
+}
+
+const Toast: React.FC<ToastProps> = () => {};
 
 export default Toast;
