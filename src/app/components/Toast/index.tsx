ca6cdb468a3d56ddea6bf00367d8af5025c05f0d
Jackson <Jacksonwrk3@gmail.com>
2024-10-16 18:11:29 -0400

    Created switch statement to make bgColor and iconURL dynamic

--- src/app/components/Toast/index.tsx
+++ src/app/components/Toast/index.tsx
@@ -12,6 +12,28 @@ interface ToastProps {
   description?: string;
 }
 
-const Toast: React.FC<ToastProps> = () => {};
+const Toast: React.FC<ToastProps> = ({ status, title, description }) => {
+  let bgColor;
+  let iconURL;
+  switch (status) {
+    case "success":
+      bgColor = "bg-green-500";
+      iconURL = "public/check.png";
+      break;
+    case "warning":
+      bgColor = "bg-amber-500";
+      iconURL = "pubic/alert-orange.png";
+      break;
+    case "info":
+      bgColor = "bg-sky-500";
+      iconURL = "public/info.png";
+      break;
+    case "error":
+      bgColor = "bg-red-500";
+      iconURL = "public/alert-red.png";
+      break;
+  }
+  return <div className={`${bgColor}`}></div>;
+};
 
 export default Toast;
