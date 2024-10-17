8904ea49f2d883717af77a8f7bc19249a53363d8
Jackson <Jacksonwrk3@gmail.com>
2024-10-16 21:02:57 -0400

    Changed id from number to string

--- src/app/context/Toast/interface.ts
+++ src/app/context/Toast/interface.ts
@@ -1,10 +1,10 @@
 /**
  * @interface Toast
  * @description Interface for a single toast notification
- * @property {number} id - The unique identifier for the toast created using Date.now()
+ * @property {string} id - The unique identifier for the toast created using Date.now()
  * @property {React.ReactElement} component - The toast component that'll be appended to the toast list
  */
 export interface Toast {
-  id: number;
+  id: string;
   component: React.ReactElement;
 }
