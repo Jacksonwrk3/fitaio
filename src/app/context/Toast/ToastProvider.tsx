f4af0dee16676a8c57e6fd28e5acc026d0a12496
Jackson <Jacksonwrk3@gmail.com>
2024-10-16 21:04:34 -0400

    Fixed Toast not disappearing bug

--- src/app/context/Toast/ToastProvider.tsx
+++ src/app/context/Toast/ToastProvider.tsx
@@ -2,10 +2,12 @@
 import ToastContext from "./ToastContext";
 import React, { ReactElement, useState } from "react";
 import { Toast } from "./interface";
+import { nanoid } from "nanoid";
 
 /**
  * @interface ToastProviderProps interface for ToastProvider props
  * @property {React.ReactNode} children
+ * @TODO center tosts for mobile, and have it on bottom right for bigger screens
  */
 
 interface ToastProviderProps {
@@ -23,7 +25,7 @@ export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
   //after 5 seconds
   const openToast = (component: ReactElement, timeout = 5000) => {
     //Creates a unique id for the toast element since it'll be rendered in a list
-    const id = Date.now();
+    const id = nanoid();
     const toastArray = [...toasts, { id, component }];
     setToasts(toastArray);
     setTimeout(() => closeToast(id), timeout);
@@ -31,13 +33,8 @@ export const ToastProvider: React.FC<ToastProviderProps> = ({ children }) => {
 
   //Function removes the item that has an id that matches the argument and sets the
   //new array with the removed item as the new toast state
-  const closeToast = (id: number) => {
-    //Returns an array WITHOUT the item of the id provided
-    const filteredArray = toasts.filter((toast) => {
-      toast.id === id;
-    });
-    //Changes toast state to the new array with the removed item
-    setToasts(filteredArray);
+  const closeToast = (id: string) => {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
   };
 
   return (
