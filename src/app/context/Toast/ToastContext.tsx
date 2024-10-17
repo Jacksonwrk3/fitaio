68d03480aec5a7a1cf1bb111225408d5ffa960d6
Jackson <Jacksonwrk3@gmail.com>
2024-10-16 21:05:16 -0400

    Changed id type from number to string

--- src/app/context/Toast/ToastContext.tsx
+++ src/app/context/Toast/ToastContext.tsx
@@ -3,15 +3,15 @@ import { Toast } from "./interface";
 
 /**
  * @description ToastContext provides a context for managing toast notifications in the application.
- * @type {React.Context<{ openToast: (component: React.ReactElement, timeout: number) => void, closeToast: (id: number) => void }>}
+ * @type {React.Context<{ openToast: (component: React.ReactElement, timeout: number) => void, closeToast: (id: string) => void }>}
  * @exports ToastContext
  */
 const ToastContext = createContext<{
   openToast: (component: React.ReactElement, timeout: number) => void;
-  closeToast: (id: number) => void;
+  closeToast: (id: string) => void;
 }>({
   openToast: (component: React.ReactElement, timeout: number) => {},
-  closeToast: (id: number) => {},
+  closeToast: (id: string) => {},
 });
 
 export default ToastContext;
