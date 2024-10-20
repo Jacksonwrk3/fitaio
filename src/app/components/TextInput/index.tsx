c32cb9f064719a128881a8fdb4a47ab70f3c01c5
Jackson <Jacksonwrk3@gmail.com>
2024-10-20 18:32:58 -0400

    refactored textinput component

--- src/app/components/TextInput/index.tsx
+++ src/app/components/TextInput/index.tsx
@@ -5,17 +5,13 @@ import { useState, useEffect } from "react";
  * @TODO write tests
  * @typedef {Object} TextInputProps - The props for the TextInput component
  * @property {string} id - The value for
- * - the id attribute of the input element
- * - the htmlFor attribute of the label element
- * - the name attribute of the input element
- * @property {React.ReactNode} children - The content inside the label element
  * @property {string} placeholder - The placeholder for the input element
  * @property {(e: React.ChangeEvent<HTMLInputElement>) => void} onChange - The function to be called when the input value changes
  * @property {string} value - The value of the input element
+ * @property {"text" | "password"} - Variant of text input
  */
 type TextInputProps = {
   id: string;
-  children: React.ReactNode;
   placeholder?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   value: string;
@@ -24,13 +20,19 @@ type TextInputProps = {
 
 /**
  * @description The TextInput component
- * @dev [Props]type - Is defaulted to "text"
- * @type {React.FC<TextInputProps>}
+ * @param {string} id - The value for
+ *  - the id attribute of the input element
+ *  - the htmlFor attribute of the label element
+ *  - the name attribute of the input element
+ * @param {string} placeholder - The placeholder for the input element
+ * @param {(e: React.ChangeEvent<HTMLInputElement>) => void} onChange - The function to be called when the input value changes
+ * @param {string} value - The value of the input element
+ * @param {string} type - "text" | "password"
+ *  - Defaulted to text
  * @returns {JSX.Element} The rendered TextInput element
  */
 const TextInput: React.FC<TextInputProps> = ({
   id,
-  children,
   placeholder,
   onChange,
   value,
@@ -49,31 +51,26 @@ const TextInput: React.FC<TextInputProps> = ({
     }
   }, []);
   return (
-    <div className="flex flex-col space-y-1">
-      <label className="font-bold" htmlFor={id}>
-        {children}
-      </label>
-      <div className=" relative flex items-center border border-grayPrimary">
-        <input
-          type={isVisible ? "text" : "password"}
-          id={id}
-          name={id}
-          className=" rounded py-2 pl-4 w-full"
-          placeholder={placeholder}
-          onChange={(e) => {
-            onChange(e);
-          }}
-          value={value}
-        />
-        {type === "password" && (
-          <div
-            className="text-blue-500 absolute right-0 mr-4 cursor-pointer"
-            onClick={toggleVisibility}
-          >
-            {isVisible ? "Hide" : "Show"}
-          </div>
-        )}
-      </div>
+    <div className=" relative flex items-center border border-grayPrimary">
+      <input
+        type={isVisible ? "text" : "password"}
+        id={id}
+        name={id}
+        className=" rounded py-2 pl-4 w-full"
+        placeholder={placeholder}
+        onChange={(e) => {
+          onChange(e);
+        }}
+        value={value}
+      />
+      {type === "password" && (
+        <div
+          className="text-blue-500 absolute right-0 mr-4 cursor-pointer"
+          onClick={toggleVisibility}
+        >
+          {isVisible ? "Hide" : "Show"}
+        </div>
+      )}
     </div>
   );
 };
