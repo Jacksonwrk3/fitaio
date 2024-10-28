2ea9376cc54d91f97c6f797203d74241c3a29259
Jackson <Jacksonwrk3@gmail.com>
2024-10-27 22:45:47 -0400

    daily commit

--- src/app/(Main)/user/workouts/page.tsx
+++ src/app/(Main)/user/workouts/page.tsx
@@ -6,6 +6,7 @@ import { Button, Modal, Toast, Text } from "@/app/components";
 import { useToast } from "@/app/hooks/useToast";
 import Link from "next/link";
 const Workouts = () => {
+  //daily commit
   const [displayModal, setDisplayModal] = useState(false);
   const session = useContext(SessionContext);
   const router = useRouter();
