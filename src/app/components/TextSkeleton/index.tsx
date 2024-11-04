db46c9715f350520a351310251bc28a1da4a63fd
Jackson <Jacksonwrk3@gmail.com>
2024-11-03 23:58:07 -0500

    Fixed typo from loadinGray to loadingGray

--- src/app/components/TextSkeleton/index.tsx
+++ src/app/components/TextSkeleton/index.tsx
@@ -1,8 +1,8 @@
 /**
- * @description Fallback component for when aysnchronous text is loading
+ * @description A gray div that pulses. Used as fallback component when async text is loading
  */
 const TextSkeleton = () => {
-  return <div className="w-full animate-pulse p-2 bg-loadinGray "></div>;
+  return <div className="w-full animate-pulse p-3 bg-loadingGray "></div>;
 };
 
 export default TextSkeleton;
