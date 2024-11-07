5fc56b70e530c979375043bcb4d78cae63d97347
Jackson <Jacksonwrk3@gmail.com>
2024-11-06 21:29:20 -0500

    Created: Helper function to capitalize first letter of string

---
+++ src/app/util/stringUtils.ts
@@ -0,0 +1,10 @@
+/**
+ * @function capitalizeFirstLetter
+ * @property {string} val - Value to manipulate
+ * @returns {string} - The string input with first letter capitalized
+ */
+function capitalizeFirstLetter(val: string) {
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+}
+
+export { capitalizeFirstLetter };
