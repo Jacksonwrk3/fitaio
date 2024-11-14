03911914e995ad6908de700204fd3d4520f5f80c
Jackson <Jacksonwrk3@gmail.com>
2024-11-13 22:18:28 -0500

    login page done

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,17 +3,46 @@ import { on } from "events";
 import { Button, TextInput, Toast } from "../../components/index";
 import { googleSignUp } from "@/app/actions/auth/index";
 import { useToast } from "@/app/hooks";
+import { createClient } from "@/app/util/supabase/client";
 import Link from "next/link";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 const SignUp = () => {
-  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const usernameOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
+  const supabase = createClient();
+  const router = useRouter();
+  const { openToast } = useToast();
+  const emailOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
   };
 
   const passwordOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
+  };
+
+  const customSignIn = async () => {
+    const { error, data } = await supabase.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
+
+    if (error) {
+      console.log("got error");
+      openToast(
+        <Toast
+          status="error"
+          title="Login Failed"
+          description={error.message}
+        />,
+        3000
+      );
+    }
+
+    if (data) {
+      console.log("should redirect");
+      router.replace("/user/workouts");
+    }
   };
 
   const googleLogin = async () => {
@@ -29,23 +58,37 @@ const SignUp = () => {
       <div className="max-w-lg w-full">
         <h1 className=" font-bold text-4xl text-center">Log In</h1>
         <form className="border-2 border-grayPrimary rounded px-6 py-8 m-2  space-y-6">
-          <TextInput
-            id="username"
-            value={username}
-            onChange={(e) => {
-              usernameOnChange(e);
-            }}
-          />
-          <TextInput
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => {
-              passwordOnChange(e);
-            }}
-          />
+          <div>
+            <label htmlFor="email">Email</label>
+            <TextInput
+              id="email"
+              value={email}
+              onChange={(e) => {
+                emailOnChange(e);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <TextInput
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                passwordOnChange(e);
+              }}
+            />
+          </div>
           <div className="space-y-2 flex flex-col">
-            <Button width="full">Log in </Button>
+            <Button
+              width="full"
+              onClick={(e) => {
+                e.preventDefault();
+                customSignIn();
+              }}
+            >
+              Log in
+            </Button>
             <p className="text-center">
               Dont have an account?{" "}
               <span className="underline">
