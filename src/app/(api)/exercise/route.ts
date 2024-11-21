13a11d5fc376ab34008062c284dd235dd00345cc
Jackson <Jacksonwrk3@gmail.com>
2024-11-20 22:08:44 -0500

    Created exercise api that takes name query param and returns back 5 exercises matching that

--- src/app/(api)/exercise/route.ts
+++ src/app/(api)/exercise/route.ts
@@ -1,8 +1,32 @@
 import { createClient } from "@/app/util/supabase/server";
-import { NextResponse } from "next/server";
-export async function GET() {
+import { NextResponse, type NextRequest } from "next/server";
+//Initialize supabase client
+
+export async function GET(req: NextRequest) {
   const supabase = createClient();
-  console.log("Inside exercise");
-  const { data, error } = await supabase.from("exercise").select().limit(10);
-  return NextResponse.json({ data }, { status: 200 });
+  try {
+    const searchParams = req.nextUrl.searchParams;
+    const name = searchParams.get("name");
+    //If name param value not provided return an error
+    if (!name) {
+      return NextResponse.json(
+        { message: "Search value is required" },
+        { status: 400 }
+      );
+    } else {
+      const { data, error } = await supabase
+        .from("exercise")
+        .select()
+        .ilike("exercise", `%${name}%`)
+        .range(0, 5);
+
+      if (error) {
+        throw new Error(error.message);
+      } else {
+        return NextResponse.json({ exercise: data }, { status: 200 });
+      }
+    }
+  } catch (error) {
+    return NextResponse.json({ message: error.message }, { status: 500 });
+  }
 }
