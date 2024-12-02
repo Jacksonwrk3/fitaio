33c3ef1b0a7151373ba63c554231f3c0e83661b8
Jackson <Jacksonwrk3@gmail.com>
2024-12-02 12:36:57 -0500

    Docs for Exercise interface

--- src/app/(Main)/user/workouts/new/feature/ExerciseSearchBar.tsx
+++ src/app/(Main)/user/workouts/new/feature/ExerciseSearchBar.tsx
@@ -16,6 +16,28 @@ interface ExerciseSearchBarProps {
 }
 
 /**
+ * @interface exercise
+ * @property {string} difficulty_level - How hard the exercise is
+ * @property {string} exercise - Name of exercise
+ * @property {id} number - Id of the workout in database
+ * @property {string | null} long_yt_vid - Link of the LONG form youtube tutorial if provided, NULL if none
+ * @property {string | null} secondary_muscle - The second muscle the exercise targets
+ * @property {string | null} short_yt_vid - Link of the SHORT form youtube tutorial if provided, NULL if none
+ * @property {string} target_muscle - Main muscle the exercise targets
+ * @property {string | null} tertiary_muscle - Third muscle target, if any
+ */
+interface Exercise {
+  difficulty_level: string;
+  exercise: string;
+  id: number;
+  long_yt_vid: string | null;
+  secondary_muscle: string | null;
+  short_yt_vid: string | null;
+  target_muscle: string;
+  tertiary_muscle: string | null;
+}
+
+/**
  * @description Search Bar that gives suggestions as you type
  * @param {string} id - ID for TextInput
  * @param {(val: string) => void | Promise<void>} onSearch - Query Function that runs when TextInput changes
@@ -88,17 +110,6 @@ const ExerciseSearchBar: React.FC<ExerciseSearchBarProps> = ({ id }) => {
     </div>
   );
 };
-
-interface Exercise {
-  difficulty_level: string;
-  exercise: string;
-  id: number;
-  long_yt_vid: string | null;
-  secondary_muscle: string | null;
-  short_yt_vid: string | null;
-  target_muscle: string;
-  tertiary_muscle: string | null;
-}
 
 // Fetches exercise from api
 const fetchExercises = async (searchValue: string) => {
